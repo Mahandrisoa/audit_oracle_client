d857885a784d44f92cd46371057f3ecd634e8093
Mahandrisoa <andriamaherisoamahandrisoa@gmail.com>
2018-03-04 07:05:07 +0300

    before presentation

--- dist/components/audit/audit.js
+++ dist/components/audit/audit.js
@@ -16,28 +16,76 @@ function auditCtrl($scope, $http, $element, $timeout, $interval, $attrs, auditSe
 
         $scope.datePicker = new Date();
 
-        ctrl.interval = setInterval(function () {
-            auditService
-                .getAudits(converted)
-                .then(function (response) {
-                    response.data.forEach(function(element) {
-                        var cache = parseFloat(element[3]).toFixed(3);
-                        element.CACHE_HIT_RATIO = cache;
-                        element.AUDIT_LABEL = element[2].substring(0, 2) + "" + element[1];
-                    });
-                    response.data.sort(compareByLabel);
-                    ctrl.httpDone = true;
-                    ctrl.audits = response.data;
-                    ctrl._temp = response.data; // temp is used for backing up data
-                    ctrl._backup = response.data;
-                    if (ctrl.audits.length > 0) {
-                        ctrl.dateAudit = ctrl.audits[0][4]; // ctrl.audits[0][4] est la date de l'audit 
-                    }
-                    ctrl.showSpinner = false;
-                }, function (reason) {
-                    console.log(reason);
+        auditService
+            .getAudits(converted)
+            .then(function (response) {
+                response.data.forEach(function (element) {
+                    var cache = parseFloat(element[3]).toFixed(3);
+                    element.CACHE_HIT_RATIO = cache;
+                    element.AUDIT_LABEL = element[2].substring(0, 2) + "" + element[1];
                 });
-        }, ctrl.timeOut);
+                response.data.sort(compareByLabel);
+                ctrl.httpDone = true;
+                ctrl.audits = response.data;
+                ctrl._temp = response.data; // temp is used for backing up data
+                ctrl._backup = response.data;
+                if (ctrl.audits.length > 0) {
+                    ctrl.dateAudit = ctrl.audits[0][4]; // ctrl.audits[0][4] est la date de l'audit 
+                } else {
+                    ctrl.dateAudit = cov(new Date().toDateString());
+                }
+                ctrl.showSpinner = false;
+
+                var sumUs = 0, sumAg = 0;
+                ctrl.audits.forEach(function (value, index) {
+                    if (value[2] === "Agence") sumAg++;
+                    else if (value[2] === "Usine") sumUs++;
+                });
+
+                $('#labelUsine').html(sumUs + ' USINES');
+                $('#labelAgence').html(sumAg + ' AGENCES');
+
+            }, function (reason) {
+                console.log(reason);
+            });
+
+        if (ctrl.enableReload) {
+            var time = parseInt($scope.refreshModel);
+            ctrl.interval = setInterval(function () {
+                auditService
+                    .getAudits(converted)
+                    .then(function (response) {
+                        response.data.forEach(function (element) {
+                            var cache = parseFloat(element[3]).toFixed(3);
+                            element.CACHE_HIT_RATIO = cache;
+                            element.AUDIT_LABEL = element[2].substring(0, 2) + "" + element[1];
+                        });
+                        response.data.sort(compareByLabel);
+                        ctrl.httpDone = true;
+                        ctrl.audits = response.data;
+                        ctrl._temp = response.data; // temp is used for backing up data
+                        ctrl._backup = response.data;
+                        if (ctrl.audits.length > 0) {
+                            ctrl.dateAudit = ctrl.audits[0][4]; // ctrl.audits[0][4] est la date de l'audit 
+                        } else {
+                            ctrl.dateAudit = cov(new Date().toDateString());
+                        }
+                        ctrl.showSpinner = false;
+
+                        var sumUs = 0, sumAg = 0;
+                        ctrl.audits.forEach(function (value, index) {
+                            if (value[2] === "Agence") sumAg++;
+                            else if (value[2] === "Usine") sumUs++;
+                        });
+
+                        ctrl.agences = sumAg;
+                        ctrl.usines = sumUs;
+
+                    }, function (reason) {
+                        console.log(reason);
+                    });
+            }, time);
+        }
     }
 
     function compareByLabel(a, b) {
@@ -58,6 +106,7 @@ function auditCtrl($scope, $http, $element, $timeout, $interval, $attrs, auditSe
         return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('/');
     }
 
+
     this.handleInterval = function () {
         if ($scope.refreshModel === "0") {
             clearInterval(ctrl.interval);
@@ -65,12 +114,12 @@ function auditCtrl($scope, $http, $element, $timeout, $interval, $attrs, auditSe
             var time = parseInt($scope.refreshModel);
             clearInterval(ctrl.interval);
             ctrl.interval = setInterval(function () {
-                var converted = cov($scope.datePicker);
+                var converted = cov(new Date().toDateString());
                 ctrl.showSpinner = true
                 auditService
                     .getAudits(converted)
                     .then(function (response) {
-                        response.data.forEach(function(element) {
+                        response.data.forEach(function (element) {
                             var cache = parseFloat(element[3]).toFixed(3);
                             element.CACHE_HIT_RATIO = cache;
                             element.AUDIT_LABEL = element[2].substring(0, 2) + "" + element[1];
@@ -82,8 +131,27 @@ function auditCtrl($scope, $http, $element, $timeout, $interval, $attrs, auditSe
                         ctrl._backup = response.data;
                         if (ctrl.audits.length > 0) {
                             ctrl.dateAudit = ctrl.audits[0][4]; // ctrl.audits[0][4] est la date de l'audit 
+                            $('#datePicker').val(null);
+                        } else {
+                            ctrl.dateAudit = cov(new Date().toDateString());
+                            var now = new Date();
+                            var day = ("0" + now.getDate()).slice(-2);
+                            var month = ("0" + (now.getMonth() + 1)).slice(-2);
+                            var today = now.getFullYear() + "-" + (month) + "-" + (day);                                                       
+                            ctrl.datePicker = null;
+                            $('#datePicker').val(null);
                         }
                         ctrl.showSpinner = false;
+
+                        var sumUs = 0, sumAg = 0;
+                        ctrl.audits.forEach(function (value, index) {
+                            if (value[2] === "Agence") sumAg++;
+                            else if (value[2] === "Usine") sumUs++;
+                        });
+
+                        ctrl.agences = sumAg;
+                        ctrl.usines = sumUs;
+
                     }, function (reason) {
                         console.log(reason);
                     });
@@ -91,13 +159,13 @@ function auditCtrl($scope, $http, $element, $timeout, $interval, $attrs, auditSe
         }
     }
 
-    this.handlePickerChange = function () {
+    this.handlePickerChange = function (e) {
         var converted = this.convertDate($scope.datePicker);
         ctrl.showSpinner = true
         auditService
             .getAudits(converted)
             .then(function (response) {
-                response.data.forEach(function(element){
+                response.data.forEach(function (element) {
                     var cache = parseFloat(element[3]).toFixed(3);
                     element.CACHE_HIT_RATIO = cache;
                     element.AUDIT_LABEL = element[2].substring(0, 2) + "" + element[1];
@@ -110,10 +178,21 @@ function auditCtrl($scope, $http, $element, $timeout, $interval, $attrs, auditSe
                 if (ctrl.audits.length > 0) {
                     ctrl.dateAudit = ctrl.audits[0][4];
                 } else {
-                    ctrl.dateAudit = converted;
+                    ctrl.dateAudit = converted;                    
                 }
                 ctrl.showSpinner = false;
                 $scope.typeModel = 'Tout';
+
+                var sumUs = 0, sumAg = 0;
+                ctrl.audits.forEach(function (value, index) {
+                    if (value[2] === "Agence") sumAg++;
+                    else if (value[2] === "Usine") sumUs++;
+                });
+                
+                ctrl.agences = sumAg;
+                ctrl.usines = sumUs;
+               
+
             }, function (reason) {
                 console.log(reason);
             });
